b740e64706d1c2ae4c776b55b144ed357d9c6c01
davonbl-bl <davonbl@gmail.com>
2023-05-11 10:02:31 -0400

    fixed up the pressDown functionality, and changed the background color of the application

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ function enterForGames(){
     if(!isNaN(getNumber) && getNumber > 0){
         numOfGames.disabled = true;
         console.log(getNumber)
+        displayNumber.innerText = getNumber; 
         return getNumber; 
     }
 
@@ -47,8 +48,6 @@ function clickForGames(){
     const getValue = chooseNum.value;
 
     displayNumber.innerText = getValue; 
-    // console.log(event)
-    // beginGame(getValue)
 
     if(!isNaN(getValue) && getValue > 0){
         numOfGames.disabled = true;
@@ -59,39 +58,9 @@ function clickForGames(){
     console.log('getting a very interesting error')
     numOfGames.disabled = false;
     console.log(parseInt(getValue))
-    // return getValue; 
 
-    // if(event.isTrusted === true && getValue !== ''){
-    //     console.log('testing')
-    //     numOfGames.disabled = true;
-    //     // beginGame(getValue)
-    //     return getValue; 
-    //     //best to create
-    //     console.log('other testing')
-    // }
-
-
-
-    // if(numOfGames.disabled === false){
-    //     console.log('hello')
-    //      
-    // } else {
-    //     console.log('it is not working')
-    // }
-    // console.log(getValue); 
-
-    // return getValue; 
-
-    //studying human psychology and the dynamatic 
     
 }
-
-// numOfGames.addEventListener('pressdown')
-
-// const numOfGames = () => {
-
-// }
-
 
 const cpuChooses = () => {
     const cpuChoice = Math.floor(Math.random() * 99);
@@ -99,21 +68,16 @@ const cpuChooses = () => {
     let cpuChoiceImg = document.querySelector('#appendCpuImg');
     cpuChoiceImg.style.width = '70%'
     console.log(cpuChoice);
-    // console.log('hello world')
 
     if(cpuChoice <= 33){
-        // console.log('rock');
-
         cpuChoiceImg.src = "./images/rock-2.jpg";
         shareCpuChoice = 'rock'
 
     } else if(cpuChoice <= 66){
-        // console.log('paper');
         cpuChoiceImg.style.width = '40%'
         cpuChoiceImg.src = "./images/paper.jpg";
         shareCpuChoice = 'paper'
     }else if(cpuChoice <= 99){
-        // console.log('scissors');
         cpuChoiceImg.src = "./images/scissors.jpg";
         shareCpuChoice = 'scissors'
     } else{
@@ -124,10 +88,9 @@ const cpuChooses = () => {
         option: shareCpuChoice,
         optionImg : cpuChoiceImg
     }
-    // const cpuChoice = document.querySelector('#clickRock');
 }
 
-// cpuChooses();
+
 const rock = document.querySelector('#clickRock');
 const paper = document.querySelector('#clickPaper');
 const scissors = document.querySelector('#clickScissors');
@@ -169,9 +132,6 @@ function beginGame(numofGames){
         click: clickForGame,
         press: pressForGame
     }
-
-    // console.log('TESTING: ' + clickForGame)
-    // console.log('TESTING2: ' + pressForGame)
 }
 
 let cpuScore = 0; 
@@ -183,28 +143,20 @@ let getCurrentWinner = document.querySelector('#getWinner')
 
 function cpuVsPlayer(playerChoice){
 
-    // let cpuDisplayScore = document.querySelector('#cpuNumbers');
-    // let playerDisplayScore = document.querySelector('#playerNumbers');
-    
-    
 
     let cpuOption = cpuChooses();
 
     if(playerChoice === 'rock' && cpuOption.option === 'scissors'
     || playerChoice === 'scissors' && cpuOption.option === 'paper'
     || playerChoice === 'paper' && cpuOption.option === 'rock'){
-        // debugger; 
         playerScore = playerScore + 1;
         console.log('player score: ' + playerScore)
-        // playerDisplayScore.innerText = parseInt(playerChoice)
         playerDisplayScore.innerText = playerScore
         getCurrentWinner.innerText = 'Player One Wins';
 
         soHumanPlayer(playerScore)
         return playerChoice
-        // getWinner(playerScore)
 
-        // return playerScore
         }else if(cpuOption.option === 'rock' && playerChoice === 'scissors'
         || cpuOption.option === 'scissors' && playerChoice === 'paper'
         || cpuOption.option === 'paper' && playerChoice === 'rock'){
@@ -212,30 +164,19 @@ function cpuVsPlayer(playerChoice){
             cpuDisplayScore.innerText = cpuScore
             getCurrentWinner.innerText = 'CPU Wins';
             soCpuPlayer(cpuScore)
-            // getWinner(cpuScore)
-            // getWinner()
-            // return cpuScore
+
             return cpuScore
         }else{
             console.log('tie')
             getCurrentWinner.innerText = "It's a tie";
-            // getWinner()
         }
 
-        // return{
-        //     humanPlayer: praseInt(playerScore),
-        //     cpuPlayer: parseInt(cpuScore)
-        // }
 
 }
 
 function soHumanPlayer(getWinner){
     const reachThisScore = parseInt(clickForGames());
     const reachThisScore2 = parseInt(enterForGames());
-
-    // const resetGame = pressForGames.value; 
-    // const clickForScore = reachThisScore.click
-    // const pressForScore = reachThisScore.press
 
     console.log(typeof clickForGames())
     console.log(reachThisScore)
@@ -245,7 +186,6 @@ function soHumanPlayer(getWinner){
 
     // console.log('num of Games --->' + getValue)
 
-    // const getTheWinner = cpuVsPlayer(); 
     let officalPlayerScore = getWinner; 
     let clearImg = document.querySelector('#appendImg')
     let cpuChoiceImg = document.querySelector('#appendCpuImg');
@@ -272,22 +212,14 @@ function soHumanPlayer(getWinner){
 function soCpuPlayer(getWinner){
     const reachThisScore = parseInt(clickForGames());
     const reachThisScore2 = parseInt(enterForGames()); 
-    // const clickForScore = reachThisScore.click;
-    // const pressForScore = reachThisScore.press;
-    // const resetGame = pressForGames.value; 
-    // console.log('TRACKING GAME: ' + resetGame)
 
-    // const getTheWinner = getWinner;  
     let officalCpuScore = getWinner; 
 
-    // console.log(typeof beginGame())
-    // console.log('what is it:::' + beginGame())
     console.log(reachThisScore)
 
     const chooseNum = document.querySelector('#decideGames');
     let getValue = chooseNum.value;
 
-    // console.log('num of Games --->' + getValue)
 
     let cpuChoiceImg = document.querySelector('#appendCpuImg');
     let clearImg = document.querySelector('#appendImg')
@@ -313,34 +245,10 @@ function soCpuPlayer(getWinner){
     }
 }
 
-// function getWinner(){
-//     const reachThisScore = beginGame();
-//     const clickForScore = reachThisScore.click
-//     console.log(clickForScore)
-//     const pressForScore = reachThisScore.press
-
-//     const getTheWinner = cpuVsPlayer(); 
-//     let officalPlayerScore = getTheWinner.humanPlayer;
-//     let officalCpuScore = getTheWinner.cpuPlayer;
-
-//     if(officalCpuScore === clickForScore){
-//         alert('CPU WINS THE GAME');
-//     } 
-//     // else if(officalPlayerScore === clickForScore || officalPlayerScore === pressForScore){
-//     //     alert('PLAYER WINS THE GAME')
-//     // } 
-    
-//     else{
-//         console.log('the game is still going')
-//     }
-
-// }
 
 /*
 
-lustful sex - let the fuck go
 
-ersioious spot
 
 comparing the conditional statements
 --clicking the amount of games
@@ -348,10 +256,6 @@ comparing the conditional statements
 -- keep in mind to reset game and active the choose number of games
 once again 
 
-
-
---> having a particular aura
---> dealing with them in terms of energy 
 
 */
 
